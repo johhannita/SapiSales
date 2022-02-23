f59bcb6578e0a03493201c22a7e9d0d97ea2f29c
johhannita <rigmanyi.eva@student.ms.sapientia.ro>
2022-02-23 14:01:15 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/SapiSales.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SapiSales.dir/main.c.obj"
+  "SapiSales.exe"
+  "SapiSales.exe.manifest"
+  "SapiSales.pdb"
+  "libSapiSales.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SapiSales.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SapiSales.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SapiSales.
+# This may be replaced when dependencies are built.
